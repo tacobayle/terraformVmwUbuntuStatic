75c0281371e545477615e331cbddf3e7557e8366
tacobayle <nicolas.bayle@gmail.com>
2021-11-25 13:47:00 +0000

    20211125_static_dhcp_use_case

--- output.tf
+++ output.tf
@@ -1,7 +1,11 @@
-output "jump" {
-  value = vsphere_virtual_machine.ubuntu.default_ip_address
+output "ubuntu_static_ips" {
+  value = vsphere_virtual_machine.ubuntu_static.*.default_ip_address
 }
 
-output "ssh_private_key_path" {
-  value = "~/.ssh/${var.ssh_key.private_key_basename}.pem"
+output "ubuntu_dhcp_ips" {
+  value = vsphere_virtual_machine.ubuntu_dhcp.*.default_ip_address
 }
+
+//output "ssh_private_key_path" {
+//  value = "~/.ssh/${var.ssh_key.private_key_basename}.pem"
+//}
