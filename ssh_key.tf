bacae25fddf2b52b97488003457fec3a5fb72223
tacobayle <nicolas.bayle@gmail.com>
2021-11-25 22:01:42 +0000

    20211125_static_dhcp_use_case

--- ssh_key.tf
+++ ssh_key.tf
@@ -6,6 +6,6 @@ resource "tls_private_key" "ssh" {
 
 resource "local_file" "private_key" {
   content         = tls_private_key.ssh.private_key_pem
-  filename        = pathexpand("~/.ssh/${var.ssh_key.private_key_basename}${random_string.id.result}.pem")
+  filename        = pathexpand("~/.ssh/${var.ssh_key.private_key_basename}.pem")
   file_permission = var.ssh_key.file_permission
 }
